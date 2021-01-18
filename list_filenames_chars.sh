b9568e82542997d0e57d2155442c88433746a45f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-01-18 23:08:28 +0200

    Auto commit

---
+++ list_filenames_chars.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## created on 2018-12-19
+
+#### list and count file names characters 
+## Usefull for detecting problematic characters or chars to clean
+
+folder="$1"
+
+if [[ ! -d "$folder" ]];then
+    echo "Give a folder to process"
+    exit
+fi
+
+(
+find "$folder" -type f -iname "*.*" | while read line;do
+    filename="$(basename "${line}")"
+    echo "$filename" | sed 's/\(.\)/\1\n/g'
+done
+) | sort | uniq -c 
+
+
+exit 0 
