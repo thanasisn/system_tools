7f1bc022506029d9ea161f81244739165260950a
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-01-19 18:07:13 +0200

    Auto commit

---
+++ count_word_frequency.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+## created on 2019-10-27
+
+#### Count word frequency
+## works for multiple languages
+
+# 1. Substitute all non alphanumeric characters with a blank space.
+# 2. All line breaks are converted to spaces also.
+# 3. Reduces all multiple blank spaces to one blank space
+# 4. All spaces are now converted to line breaks. Each word in a line.
+# 5. Convert all words to lower case
+# 6. Sorts words
+# 7. Counts and remove the equal lines
+# 8. Sorts reverse in order to count the most frequent words
+# 9. Add a line number to each word in order to know the word posotion
+
+
+AFILE="$1"
+
+if [ ! -f "$AFILE" ]; then
+    echo "Give a file"
+    exit 1
+fi
+
+## Get all words
+words="$(
+sed -e 's/[^[:alpha:]]/ /g' "$AFILE" |\
+    tr '\n' " "                      |\
+    tr '\r' " "                      |\
+    tr -s " "                        |\
+    tr " " '\n'                      |\
+    awk '{print tolower($0)}'        |\
+    sort 
+)"
+
+## Count word frequency
+echo "----------------------------"
+echo "$words" | uniq -c | sort -nr | nl
+echo "----------------------------"
+
+## Total words
+echo "Total words:  $(echo "$words" | wc -l)"
+
+
+exit 0
